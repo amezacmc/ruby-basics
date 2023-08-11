1c84b4d7fd19aacb3fda76e033aa1eb8c0ec7dc1
Alberto Meza <mezase97@gmail.com>
2023-08-11 04:31:56 -0500

    Added Loops Assessments

---
+++ assessments/loops.rb
@@ -0,0 +1,27 @@
+## Assessment 1: Simple Looping
+##   1. Create an array named `fruits` containing the following fruits: "apple", "banana", "orange", "grape", "watermelon".
+##   2. Print out the elements of the array as an enumerated list.
+
+
+## Assessment 2: Even Numbers
+##   Write a program that is able to print all even numbers between 20 to 40.
+##   Note:
+##     Must use the `while` loop
+
+
+## Assessment 3: Countdown Timer
+##   Write a Ruby program that prompts the user to enter a positive integer representign the number of seconds for the countdown.
+##   Then, use the `until` statment to display the countdown in the screen until it reaches 0.
+
+
+## Assessment 4: Fibonacci Series
+##   Write a Ruby program that displays the first 5 elements of the fibonacci series.
+##   Then prompts the user if the series should continue.
+##   if `yes`, shows the following 5 elements of the series and prompts again.
+##   if `no`, exits from the series.
+##   Use `next` and `break` statements.
+
+
+## Assessment 5: Number Guessing Game
+##   Create a number guessing game in Ruby where the computer generates a random number between 1 and 100, and the user has to guess the number.
+##   Use `do-while` loop.
