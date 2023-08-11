2dab8057ad3c99ff3b046c82c79be3087fab6445
Alberto Meza <mezase97@gmail.com>
2023-08-11 04:04:34 -0500

    Added Variables and Data Types Assessments

---
+++ assessments/variables_and_data_types.rb
@@ -0,0 +1,88 @@
+## Assessment 1: Basic Variable Declaration and Usage
+##   1. Declare two variables, `name` and `age`, and assign them your name and age.
+##   2. Print out a message using these variables: "My name is [name], I'm learning Ruby! And I am [age] years old."
+
+
+## Assessment 2: Difference between String and Symbol
+##   1. Declare two constant strings and give them the same value.
+##   2. Declare two symbols and give them the same value.
+##   3. Compare
+
+
+## Assessment 3: Numeric Operations and Conversions
+##   1. Declare a variable `int_number` with an integer value.
+##   2. Declare another variable `float_number` with a floating-point value.
+##   3. Perform the following operations:
+##     - Add `int_number` and `float_number`.
+##     - Substract `float_number` from `int_number`
+##     - Multiply `int_number` by 2.
+##     - Divide `float_number` by 3.
+##   4. Print the results of each operation.
+
+
+## Assessment 4: Numeric Operations and Conversions
+##   1. Declare a variable `int_number` with an integer value.
+##   2. Declare another variable `float_number` with a floating-point value.
+##   3. Perform the following operations:
+##     - Add `int_number` and `float_number`.
+##     - Substract `float_number` from `int_number`
+##     - Multiply `int_number` by 2.
+##     - Divide `float_number` by 3.
+##   4. Print the results of each operation.
+
+
+## Assessment 5: Working with Arrays.
+##   1. Create an array named `fruits` containing the following fruits: "apple", "banana", "orange", "grape", "watermelon".
+##   2. Add "strawberry" to the end of the array.
+##   3. Remove the second fruit from the array.
+##   4. Print out the mmodified `fruits` array.
+##   5. Print only the third fruit in the array.
+
+
+## Assessment 6: More Array Operations
+##   1. Create an array called `numbers` containing the numbers 5, 10, 15, 20 and 25.
+##   2. Calculate the sum of all the numbers in the `numbers` array.
+##   3. Calculate the product of the first and last numbers in the array.
+##   4. Replace the third number with its square.
+##   5. Print out the modified `numbers` array.
+
+
+## Assessment 7: Hash Basics
+##   1. Create an empty hash called `person`
+##   2. Add the following key-value pairs to the `person` hash and assign values:
+##     - `"name"`
+##     - `"age"`
+##   3. Print out the `person`hash
+
+
+## Assessment 8: Hash Operations
+##   1. Create a hash called `book` with the following key-value pairs:
+##     - `"title"`
+##     - `"author"`
+##     - `"year"`
+##   2. Add a key-value pair `"genre"` with the value of the book's genre to the `book` hash.
+##   3. Create a sub-hash from the `book` hash called `book_preview` containing only the title and author.
+##   4. Print out over the `book` hash and `book_preview` and print each key and its corresponding value.
+
+
+## Assessment 9: Nested Hashes
+##   1. Create a hash called `library` with two key-value pairs:
+##     - `"name"`
+##     - `"books"`
+##     ** Note: `books` must have an array with one hash item with the key-value pairs:
+##       - `"title"`
+##       - `"author"`
+##       - `"year"`
+##   2. Add an additional book to the `books` array.
+##   3. Print out the name of the library and the titles of all the books in the library.
+
+
+## Assessment 10: Conditional Statements and Boolean Logic
+##   1. Declare a variable `temperature` and assign it a value representing the current temperature in Celsius.
+##   2. Using an if-else statement, declare the following:
+##     - If `temperature` is less than 0, print "it's freezing!"
+##     - If `temperature` is between 0 and 20 (inclusive), print "It's cold."
+##     - If `temperature` is between 21 and 30 (inclusive), print "It's comfortable."
+##     - If `temperature` is above 30, print "It's hot!"
+##   3. Test the code with different `temperature` values.
+
