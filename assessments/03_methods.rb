e8bd2644f832907691fd1d995ea0c3c5b3f74144
Alberto Meza <mezase97@gmail.com>
2023-08-11 06:55:44 -0500

    Added Methods assessments

---
+++ assessments/03_methods.rb
@@ -0,0 +1,41 @@
+## Assessment 1: Basic Methods
+##   Write a Ruby Program that defines a method that when invoked the message is displayed: "I'm printing from a method!"
+
+
+## Assessment 2: Basic Data Manipulation
+##   Write a Ruby program that takes an array of integers as input and calculates the sum, average, maximum and minimum values of the numbers in the array.
+##   Each value must be calculated using methods. (Recommended: Do it twice, the first one with default parameters, the second one with keyword parameters).
+
+
+## Assessment 3: String Manipulation
+##   Write a Ruby program that takes a string as input and reverses the order of words in the string.
+
+
+## Assessment 4: Basic Conditional Looping
+##   Write a Ruby program that simulates a basic number guessing game. 
+##   The program generates a random number between 1 and 100, and the player has to guess the number. 
+##   Provide feedback to the player if their guess is too high or too low, and let them know how many attempts it took to guess the correct number.
+##   Note: Use predicate method(s).
+
+## Assessment 5: Text Analysis
+##   Write a Ruby program that reads a text file named "story.txt" and performs a basic text analysis. 
+##   Count the total number of words in the file, as well as the occurrences of each unique word. 
+##   Display the word frequency in descending order of occurrence.
+##   Note: Use Bang method(s).
+
+## Assessment 6: String Modification with Bang Methods
+##   Write a Ruby program that reads a string as input and perform the following actions:
+##     1. Remove any leading and trailing whitespace from the input.
+##     2. Convert all characters in the string to uppercase.
+##     3. Replace all ocurrences of the word "not" with "definitely".
+##     4. Remove any duplicate consecutive excalamation marks (!) from the string.
+##   Note: Use Bang method(s).
+
+
+## Assessment 7: Filtering Array with Lambda
+##   Write a Ruby program that takes an array of numbers as input and uses a lambda to filter out all the even numbers from the array.
+
+
+## Assessment 8: Custom Map Function with Proc
+##   Implement a custom my_map function that takes an array and a proc as arguments. 
+##   The function should apply the proc to each element of the array and return a new array containing the modified elements.
